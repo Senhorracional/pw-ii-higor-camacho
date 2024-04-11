b7bcfd21f49baf9337b8c3cd2de96c8dd2ae81c5
Senhorracional <higor10camacho@gmail.com>
2024-04-11 13:31:49 -0300

    Exercicio dos números primos

--- aula05/exercicio2.php
+++ aula05/exercicio2.php
@@ -1,1 +1,24 @@
 <?php
+function ContaPrimos($num1, $num2){
+    $primos = array();
+    $i = $num1;
+    if($i < $num2){
+        $i = $num1;
+    }else{
+        $i = $num2;
+    }
+    while($i <= $num1 &&  $i <= $num2){
+         switch($i){
+            case $i / $i == 0:
+            $cont = 0;
+            if($i / $i != 0){
+                $primos[$cont] = $i;
+                echo $primos[$cont];
+                $cont++;
+            }
+            $i++;
+        }
+    }
+} 
+ContaPrimos($_POST["num1"], $_POST["num2"]);
+?>
